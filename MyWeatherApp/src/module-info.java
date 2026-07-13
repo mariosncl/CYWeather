f7f1e8adddce956d9e3cdc94b8c7928cf93ec8cf
Marios Nicolaou <99167988+mariosncl@users.noreply.github.com>
2026-07-13 12:03:01 +0300

    Delete MyWeatherApp directory

--- MyWeatherApp/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module MyWeatherApp {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires org.json;
-	requires javafx.graphics;
-
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
